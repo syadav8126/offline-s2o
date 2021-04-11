bb0c485387750115f7f41ba3655c457afe9fdccc
syadav8126 <syadav8126@gmail.com>
2021-04-11 10:46:06 +0530

    Moved templates to graph submodule

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import homepage,testing, company_view
+from .views import homepage,testing
 from graph.views import Graph
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage, name='home'),
-    path('company/<str:company>',company_view, name="companypage"),
     path('test/<int:year>',testing, name='testing'),
     path('', include('graph.urls')),
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .update_db import get_prices
 
 def homepage(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
     return render(request, "home/home_tmp.html", {})
-    
-def company_view(request, company="COALINDIA"):
-    print(company)
-    context={}
-    context['df'] = get_prices(company)
-    return render(request, 'graph/df.html', context)
 
 def testing(request, year=2005):
     print(year)
